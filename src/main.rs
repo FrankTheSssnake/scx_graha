ebc2320e6b9b4d7ac2fa64de64eb485bf7c9a8f5
AB <174193320+FrankTheSssnake@users.noreply.github.com>
2026-03-17 23:37:18 +0530

    add skeleton implementation

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,92 @@ use scx_utils::libbpf_clap_opts::LibbpfOpts;
 use scx_utils::UserExitInfo;
 
 
+// default time slice
+const SLICE_NS: u64 = 5_000_000;
+
+
 struct GrahaScheduler<'a> {
     bpf: BpfScheduler<'a>
 }
 
 impl<'a> GrahaScheduler<'a> {
     fn init(open_object: &'a mut MaybeUninit<OpenObject>) -> Result<Self> {
+        let open_opts = LibbpfOpts::default();
+        let bpf = BpfScheduler::init(
+            open_object,
+            open_opts.clone().into_bpf_open_opts(),
+            0,              // buffer size of exit info
+            false,          // include all tasks
+            true,           // debug mode
+            true,           // use idle cpus
+            SLICE_NS,       // default time slice
+            "graha"
+        )?;
+
+        Ok(Self { bpf })
     }
 
+    // TODO
     fn dispatch_tasks(&mut self) {
+        let waiting = *self.bpf.nr_queued_mut();
+
+        while let Ok(Some(task)) = self.bpf.dequeue_task() {
+            let mut dispatched_task = DispatchedTask::new(&task);
+
+            // calculate best idle cpu
+            let cpu = self.bpf.select_cpu(task.pid, task.cpu, task.flags);
+            dispatched_task.cpu = if cpu >= 0 { cpu } else { RL_CPU_ANY };
+
+            dispatched_task.slice_ns = SLICE_NS / (waiting + 1);
+
+            self.bpf.dispatch_task(&dispatched_task).unwrap();
+        }
+
+        // sleep
+        self.bpf.notify_complete(0);
     }
 
     fn print_stats(&mut self) {
+        // Internal scx_rustland_core statistics.
+        let nr_user_dispatches = *self.bpf.nr_user_dispatches_mut();
+        let nr_kernel_dispatches = *self.bpf.nr_kernel_dispatches_mut();
+        let nr_cancel_dispatches = *self.bpf.nr_cancel_dispatches_mut();
+        let nr_bounce_dispatches = *self.bpf.nr_bounce_dispatches_mut();
+        let nr_failed_dispatches = *self.bpf.nr_failed_dispatches_mut();
+        let nr_sched_congested = *self.bpf.nr_sched_congested_mut();
+
+        println!(
+            "user={} kernel={} cancel={} bounce={} fail={} cong={}",
+            nr_user_dispatches,
+            nr_kernel_dispatches,
+            nr_cancel_dispatches,
+            nr_bounce_dispatches,
+            nr_failed_dispatches,
+            nr_sched_congested,
+        );
     }
 
     fn now() -> u64 {
+        SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
     }
 
     fn run(&mut self) -> Result<UserExitInfo> {
+        let mut prev_ts = Self::now();
+
+        while !self.bpf.exited() {
+            self.dispatch_tasks();
+
+            let current_ts = Self::now();
+            if current_ts > prev_ts {
+                self.print_stats();
+                prev_ts = current_ts
+            }
+        }
+
+        self.bpf.shutdown_and_report()
     }
 }
 
